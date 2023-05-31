f44141454ca7427fe8f2a1d7fe5a195d21fc0f9d
espemm <espemm@gmail.com>
2023-05-31 15:21:43 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 xhost +local:
 xhost +si:localuser:root
-docker run --security-opt apparmor:unconfined --name examen3a --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" espemm/prova:a
+docker run --security-opt apparmor:unconfined --name examen3a --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" espemm/provay
